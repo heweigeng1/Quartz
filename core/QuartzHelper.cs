bb32b03350ef8f656250305fa41b2b1968dca460
tom <6318275@qq.com>
2017-03-06 19:01:58 +0800

    初始化

---
+++ core/QuartzHelper.cs
@@ -0,0 +1,31 @@
+﻿using Quartz;
+using Quartz.Impl;
+using Quartz.Impl.Triggers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace core
+{
+    public class QuartzHelper:IJob
+    {
+        public void Execute(IJobExecutionContext context)
+        {
+            Console.WriteLine(DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss"));
+        }
+        public void star()
+        {
+            ISchedulerFactory factory = new StdSchedulerFactory();
+            IScheduler scheduler = factory.GetScheduler();
+            scheduler.Start();
+            IJobDetail job = JobBuilder.Create<QuartzHelper>().WithIdentity("QuartzHelper", "JobGroup1").Build();
+            //ITrigger trigger = TriggerBuilder.Create().StartNow().Build();
+            ITrigger trigger = new CronTriggerImpl("CronTrigger", "TriggerGroup1", "0 0 12 * * ?");
+            scheduler.ScheduleJob(job, trigger);
+            Thread.Sleep(1000*5);//延迟执行
+        }
+    }
+}
---
+++ tom.Console/Program.cs
@@ -0,0 +1,18 @@
+﻿using core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tom.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new QuartzHelper().star();
+            
+        }
+    }
+}
